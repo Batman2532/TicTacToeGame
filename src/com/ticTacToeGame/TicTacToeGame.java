eac9618c60bed80506f650284ce87a7d0e7ed162
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-05 17:51:16 +0530

    Refactor: added code to all switch cases

--- src/com/ticTacToeGame/TicTacToeGame.java
+++ src/com/ticTacToeGame/TicTacToeGame.java
@@ -48,28 +48,36 @@ public class TicTacToeGame {
                 System.out.println(board[indexNumber]);
                 break;
             case 2:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 3:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 4:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 5:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 6:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 7:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 8:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             case 9:
-                System.out.println();
+                board[indexNumber]= user;
+                System.out.println(board[indexNumber]);
                 break;
             default:
                 System.out.println("Enter valid number");
