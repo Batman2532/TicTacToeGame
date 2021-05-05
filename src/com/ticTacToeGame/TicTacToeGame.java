df67aad78a9921678c1feebb5f8db3797ba49145
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-05 14:26:35 +0530

    added: choose the user letter

--- src/com/ticTacToeGame/TicTacToeGame.java
+++ src/com/ticTacToeGame/TicTacToeGame.java
@@ -1,20 +1,38 @@
 package com.ticTacToeGame;
 
+import java.lang.constant.Constable;
+import java.util.Scanner;
+
 public class TicTacToeGame {
-
-    private static  char[] userInput;//declaring the userInput variable
-
+    
+    private static  char[] board;//declaring the userInput variable
+    public static String computer;
+    public static String user;
     public TicTacToeGame(){
-        userInput = new char[10];//initializing userInput variable
+        board = new char[10];//initializing userInput variable
     }
-    public static void CreateBoard() {
+    public static void createBoard() {
         for (int i = 0; i < 10; i++) { //storing null values in userInput using for loop
-            userInput[i] = ' ';
+            board[i] = ' ';
         }
-
+    }
+    public static void allowPlayer(){
+        String choice = "x";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x or o to select letter from");
+        choice = sc.nextLine(); //take inpute from user
+        if (choice == "x"){ // check whether the player entered x or o
+            user = "x";
+            computer = "o";
+        }
+        else{
+            user = "o";
+            computer = "x";
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to tic toe game");
-        CreateBoard(); //calling the method
+        createBoard(); //calling the createBoard method
+        allowPlayer(); //calling the allowPlayer method
     }
 }
