b920e58af2f33cf9004d025752e77be69afcb671
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-05 12:37:11 +0530

    Added: initilization of array

--- src/com/ticTacToeGame/TicTacToeGame.java
+++ src/com/ticTacToeGame/TicTacToeGame.java
@@ -2,6 +2,12 @@ package com.ticTacToeGame;
 
 public class TicTacToeGame {
 
+    int[] userInput;
+
+    public TicTacToeGame(){
+        userInput = new int[10];
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to tic toe game");
     }
