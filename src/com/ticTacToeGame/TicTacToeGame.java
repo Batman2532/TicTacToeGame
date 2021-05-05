a7743fcd2946eb2702e638ff3b68ca085a3a959c
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-05 13:29:06 +0530

    refactor: changes in decleration

--- src/com/ticTacToeGame/TicTacToeGame.java
+++ src/com/ticTacToeGame/TicTacToeGame.java
@@ -2,13 +2,19 @@ package com.ticTacToeGame;
 
 public class TicTacToeGame {
 
-    int[] userInput;
+    private static  char[] userInput;//declaring the userInput variable
 
     public TicTacToeGame(){
-        userInput = new int[10];
+        userInput = new char[10];//initializing userInput variable
     }
+    public static void CreateBoard() {
+        for (int i = 0; i < 10; i++) { //storing null values in userInput using for loop
+            userInput[i] = ' ';
+        }
 
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to tic toe game");
+        CreateBoard(); //calling the method
     }
 }
